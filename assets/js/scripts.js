3f28ce526edc4d991fac0f0660603b2461e82fed
FalcaoCamilla <millafalcao.acad@gmail.com>
2022-05-01 19:28:12 -0300

    script criado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,12 @@
-console.log("hello world!")
+var currentNumberWrapper = document.getElementById('currentNumber'); //span
+var currentNumber=0; 
+
+function increment(){
+    currentNumber=currentNumber+1;
+    currentNumberWrapper.innerHTML = currentNumber; //dentro do html
+}
+
+function decrement(){
+    currentNumber=currentNumber-1;
+    currentNumberWrapper.innerHTML = currentNumber; //dentro do html
+}
